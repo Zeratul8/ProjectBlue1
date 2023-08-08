b81dc7d1853d0b211bebbaea9ce00bec47cfe03a
rmflsdl4 <rmflsdl4@gmail.com>
2023-08-08 16:18:40 +0900

    [Flier] 임시 수정
    보완 예정

--- ProjectBlue1/Assets/Script/ExcelParsing.cs
+++ ProjectBlue1/Assets/Script/ExcelParsing.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class ExcelParsing : MonoBehaviour
 {
+    public TextAsset file;
+    private AllStatus datas;
+    private void Awake()
+    {
+        datas = JsonUtility.FromJson<AllStatus>(file.ToString());
 
+        foreach (var value in datas.status)
+        {
+            print(value.Attack);
+        }
+    }
+}
+
+[System.Serializable]
+public class AllStatus
+{
+    public Status[] status;
+}
+[System.Serializable]
+public class Status
+{
+    public float Attack;
+    public float Health;
+    public float CriticalHit;
+    public float CriticalDamage;
 }
