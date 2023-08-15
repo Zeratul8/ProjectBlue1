b87f1f30edd46c130f755b615b7401a515d9e717
rmflsdl4 <rmflsdl4@gmail.com>
2023-08-15 14:30:50 +0900

    [Flier] Seleted_View_Menu 수정

--- ProjectBlue1/Assets/Script/Seleted_View_Menu.cs
+++ ProjectBlue1/Assets/Script/Seleted_View_Menu.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Seleted_View_Menu : MonoBehaviour
 {
-    public GameObject[] obj = new GameObject[2];
-    public GameObject[] view_obj = new GameObject[4];
-    public int index;
+    public GameObject[] obj = new GameObject[2]; // 캔버스의 ElementView 자식 오브젝트 ( Menus, IMG_MenuBackground ) 오브젝트 삽입
+    private GameObject[] view_obj; // IMG_MenuBackground의 자식 오브젝트들을 삽입할 배열
+    private int index; // 선택한 메뉴 번호
 
     void Awake()
     {
-        for (int i = 0; i < view_obj.Length; i++)
+        view_obj = new GameObject[obj[1].transform.childCount]; // IMG_MenuBackground의 자식 개수만큼만 배열 생성
+
+        // IMG_MenuBackground의 자식 오브젝트 삽입
+        int idx = 0;
+        foreach (Transform t in obj[1].transform)
         {
-            view_obj[i] = obj[1].transform.GetChild(i).gameObject;
+            view_obj[idx] = t.gameObject;
+            idx++;
         }
+
         for (int i = 0; i < view_obj.Length; i++)
         {
-            if (i != 0) view_obj[i].SetActive(false);
+            if (view_obj[i].name != "Status")
+            {
+                view_obj[i].SetActive(false);
+            }
         }
     }
 
-    public void Seleted_Menu(int index)
+    public void Seleted_Menu(int index) // 선택한 메뉴만 보이기
     {
         for(int i = 0; i < view_obj.Length; i++)
         {
-            if (i == index) view_obj[i].SetActive(true);
-            else view_obj[i].SetActive(false);
+           view_obj[i].SetActive(false);
         }
+        view_obj[index].SetActive(true);
     }
 }
