64de821cac20a48d75b91d0b61455c9b9c453cf7
Zeratul8 <98034273+Zeratul8@users.noreply.github.com>
2023-09-28 01:42:53 +0900

    [파댕이] 사운드 리소스 추가

---
+++ ProjectBlue1/Assets/Script/SoundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
+{
+    [SerializeField]
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    protected override void OnStart()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
